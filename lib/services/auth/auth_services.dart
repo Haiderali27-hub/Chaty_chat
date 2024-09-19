66aab6412b5349d2334b87574ce6a72182fd65a8
Muhammad Haider Ali <111229279+Haiderali27-hub@users.noreply.github.com>
2024-09-19 15:27:25 +0500

    data store in firestore like id and email etc

--- lib/services/auth/auth_services.dart
+++ lib/services/auth/auth_services.dart
@@ -1,9 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthServices extends ChangeNotifier {
   //instance of Auth
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  //instance of firestore
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
   //sign user in
   Future<UserCredential> signInWithEmailAndPassword(
@@ -12,6 +16,13 @@ class AuthServices extends ChangeNotifier {
       //signIn
       UserCredential userCredential = await _firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
+
+// add a new document for the user in users collection if it doesn't already exists
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      }, SetOptions(merge: true));
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
@@ -24,6 +35,12 @@ class AuthServices extends ChangeNotifier {
     try {
       UserCredential userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+// after creating the user, create a new document for the user in the users collection
+      _fireStore.collection('users').doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        'email': email,
+      });
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
